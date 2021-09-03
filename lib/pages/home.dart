8559dcb7634e3b55fcaeaca2ef44592e0a71be02
Esmond Wong <ewong@hk.palo-it.com>
2021-09-03 14:45:46 +0800

    Step 2: API Call and State Logic

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,75 +5,92 @@ import 'package:weather/widgets/forecast_list.dart';
 import 'package:weather/widgets/location_app_bar.dart';
 import 'package:weather/widgets/weather_overview.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Widget Key
+  final refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
+
+  // Current Weather
+  CurrentWeatherResponse? currentWeather;
+  WeatherStation? selectedStation; // What we need
+
+  // Forecast
+  ForecastResponse? forecast; // What we need
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.scheduleFrameCallback((_) {
+      refreshIndicatorKey.currentState?.show();
+    });
+  }
+
+  /// Fetch Everything
+  Future<List<void>> fetchWeatherInfo() async {
+    return Future.wait([
+      fetchCurrentWeather(),
+      fetchForecast(),
+    ]);
+  }
+
+  Future<void> fetchCurrentWeather() async {
+    final _currentWeather = await getCurrentWeather();
+    setState(() {
+      currentWeather = _currentWeather;
+      selectedStation = _currentWeather.stations.first;
+    });
+  }
+
+  Future<void> fetchForecast() async {
+    final _forecast = await getWeatherForecast();
+    setState(() {
+      forecast = _forecast;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: Theme.of(context).backgroundDecoration,
       child: Scaffold(
-        body: ListView(
-          children: [
-            LocationAppBar(
-              stationName: CurrentWeatherResponse.mock().stations.first.name,
-            ),
-            SizedBox(height: 20), // Spacing
-            WeatherOverview(
-              selectedStation: CurrentWeatherResponse.mock().stations.first,
-              generalInfo: ForecastResponse.mock().situation,
-            ),
-            SizedBox(height: 40), // Spacing
-            ForecastList(
-              forecasts: ForecastResponse.mock().forecasts,
-            ),
-          ],
+        body: RefreshIndicator(
+          key: refreshIndicatorKey,
+          onRefresh: fetchWeatherInfo,
+          child: _buildContent(),
         ),
       ),
     );
   }
-}
 
-/// ^^^^^^ Stateless Widget ^^^^^^
-/// -------------------------------
-/// -    TUTORIAL PURPOSE ONLY    -
-/// -------------------------------
-/// vvvvvv Stateful Widget vvvvvvv
+  Widget _buildContent() {
+    final _selectedStation = selectedStation;
+    final _forecast = forecast;
 
-/// Widget class
-class DemoStatefulWidget extends StatefulWidget {
-  const DemoStatefulWidget({Key? key}) : super(key: key);
+    // No data :(
+    if (_selectedStation == null || _forecast == null) {
+      return Container();
+    }
 
-  @override
-  _DemoStatefulWidgetState createState() => _DemoStatefulWidgetState();
-}
-
-/// State class of the widget
-///
-/// Separated from the widget class for mutability
-/// since Widget is immutable and constructed everytime the Widget tree rebuilt
-class _DemoStatefulWidgetState extends State<DemoStatefulWidget> {
-  int counter =
-      0; // Internal state! Belongs to this widget `DemoStatefulWidget` only!
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(counter.toString()),
-          TextButton(
-            onPressed: incrementCounter,
-            child: const Text("Press Me!"),
-          ),
-        ],
-      ),
+    // We got data!
+    return ListView(
+      children: [
+        LocationAppBar(
+          stationName: _selectedStation.name,
+        ),
+        SizedBox(height: 20), // Spacing
+        WeatherOverview(
+          selectedStation: _selectedStation,
+          generalInfo: _forecast.situation,
+        ),
+        SizedBox(height: 40), // Spacing
+        ForecastList(
+          forecasts: _forecast.forecasts,
+        ),
+      ],
     );
-  }
-
-  ///
-  void incrementCounter() {
-    setState(() {
-      counter += 1;
-    });
   }
 }
